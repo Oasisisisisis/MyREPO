ad1976a1779f9817289f13383324da7c52343f22
LAPTOP-07KV7TBL\ACER <vivian20021213@gmail.com>
2024-01-01 22:22:54 +0800

    修改資料庫內容

--- shopping kart/online_shop.sql
+++ shopping kart/online_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-11-20 04:43:02
+-- 產生時間： 2024-01-01 15:21:31
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -24,59 +24,124 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `user_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `cart`
+--
+
+INSERT INTO `cart` (`id`, `product_id`, `quantity`, `user_id`) VALUES
+(92, 15, 3, '2'),
+(93, 16, 2, 'A'),
+(94, 17, 11, 'A'),
+(95, 21, 5, '2'),
+(96, 18, 1, 'A');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `product`
 --
 
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `remain` int(11) NOT NULL DEFAULT 0,
   `detail` text NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `cart` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`product_id`) REFERENCES `product`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 --
 -- 傾印資料表的資料 `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `price`, `remain`, `detail`) VALUES
-(1, 'coffee', 420, 76, ''),
-(2, '1', 1, 1, '1');
+INSERT INTO `product` (`id`, `name`, `price`, `remain`, `detail`, `user_id`) VALUES
+(15, 'coffee', 299, 8, 'good', '1'),
+(16, 'chocolate', 120, 22, 'goodgoodgood', '1'),
+(17, 'iphone', 50000, 11, 'iphone15', '123'),
+(18, 'happyNewYear', 2024, 28, 'hi', '123'),
+(21, 'pen', 10, 15, 'blue', '1');
 
+-- --------------------------------------------------------
 
+--
+-- 資料表結構 `user`
+--
 
-INSERT INTO `cart` (`id`, `product_id`, `quantity`) VALUES
-(1, 1, 2),  
-(2, 2, 1); 
+CREATE TABLE `user` (
+  `id` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `pwd` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `role` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- 傾印資料表的資料 `user`
+--
+
+INSERT INTO `user` (`id`, `pwd`, `role`) VALUES
+('1', '1', 99),
+('123', '123', 99),
+('2', '2', 1),
+('A', '111', 1);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- 資料表索引 `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `product`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
---
 -- 使用資料表自動遞增(AUTO_INCREMENT) `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
